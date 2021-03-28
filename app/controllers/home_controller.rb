7a5d9e250cbb217594958b9efe676931938e843c
obama <masubuti1129@yahoo.co.jp>
2021-03-28 02:28:58 +0000

    Update top.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
   def top
   end
   
+  def login
+  end
+  
+  def registration
+  end
+  
   def about
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/" => "home#top"
+  get "login" => "home#login"
+  get "registration" => "home#registration"
   get "about" => "home#about"  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
